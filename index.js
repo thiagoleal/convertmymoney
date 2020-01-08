64a0a85fd7a1ad64835e653bf1166869e2a41ed7
Thiago Leal <thfleal@gmail.com>
2020-01-08 00:39:00 -0300

    Added API to automatically gets exchange rate Code formatted to Airbnb Style Guide

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 const express = require('express');
+
 const app = express();
+
 const path = require('path');
-const port = process.env.PORT || 3000;
+
 const convert = require('./lib/convert');
+
+const apiBCB = require('./lib/api.bcb');
+
+const port = process.env.PORT || 3000;
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) =>{
-    res.render('home')
-})
+app.get('/', async (req, res) => {
+  const cotacao = await apiBCB.getCotacao();
+  res.render('home', {
+    cotacao,
+  });
+});
 
-app.get('/cotacao', (req, res) =>{
-    const { cotacao, quantidade} = req.query;
-    if(cotacao&&quantidade){
+app.get('/cotacao', (req, res) => {
+  const { cotacao, quantidade } = req.query;
+  if (cotacao && quantidade) {
     const conversao = convert.convert(cotacao, quantidade);
     res.render('cotacao', {
-        error: false,
-        cotacao: convert.toMoney(cotacao),
-        conversao: convert.toMoney(conversao),
-        quantidade: convert.toMoney(quantidade)
+      error: false,
+      cotacao: convert.toMoney(cotacao),
+      conversao: convert.toMoney(conversao),
+      quantidade: convert.toMoney(quantidade),
     });
-    }else{
-        res.render('cotacao', {
-            error: 'Valores inválidos!'
-        })
-    }
-})
+  } else {
+    res.render('cotacao', {
+      error: 'Valores inválidos!',
+    });
+  }
+});
 
-app.listen(port, err => {
-    if(err){
-        console.log("Couldn't start ConvertMyMoney");
-    }else{
-        console.log('Sucessfully started ConvertMyMoney!');
-    }
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Couldn't start ConvertMyMoney");
+  } else {
+    console.log('Sucessfully started ConvertMyMoney!');
+  }
 });
